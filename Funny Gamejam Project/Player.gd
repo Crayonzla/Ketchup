41ecd971a0cdaa981b7c3c421e225b47115d7ac1
Crayonzla <98232931+Crayonzla@users.noreply.github.com>
2023-07-05 22:30:19 -0700

    Update Player.gd

--- Funny Gamejam Project/Player.gd
+++ Funny Gamejam Project/Player.gd
@@ -10,23 +10,22 @@ const deacc = 1.2
 var motion = Vector2()
 
 func _physics_process(delta):
-	left_movement()
-	right_movement()
+	movement()
 	_jump()
 	_gravity()
+	idle()
+	print(motion.y)
 
 func idle():
-	if motion.x == 0 && motion.y == 0:
+	if motion.x == 0 && motion.y == 8:
 		$AnimationPlayer.play("Idle")
 
-func left_movement():
+func movement():
 	if Input.is_action_pressed("ui_left"):
 		$Sprite2D.flip_h = true
 		motion.x  = max(motion.x - acc, -maxspeed)
 	elif motion.x < 0:
 		motion.x = min(motion.x + deacc, 0)
-
-func right_movement():
 	if Input.is_action_pressed("ui_right"):
 		$Sprite2D.flip_h = false
 		motion.x  = min(motion.x + acc, maxspeed)
@@ -35,11 +34,16 @@ func right_movement():
 
 	set_velocity(motion)
 	move_and_slide()
+	set_up_direction(Vector2.UP)
+	set_floor_stop_on_slope_enabled(true)
+	set_max_slides(4)
+	move_and_slide()
 	motion = velocity
 
 func _jump():
-	if Input.is_action_pressed("ui_up") && motion.y == 0:
-		motion.y = min(motion.y - 1, jump)
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up") && motion.y >= 0:
+			motion.y = min(motion.y - 1, jump)
 
 func _gravity():
 	motion.y += gravity
