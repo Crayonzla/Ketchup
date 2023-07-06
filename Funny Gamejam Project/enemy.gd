47fad303422a20732f38b5f9ac883258cd5fdae6
Crayonzla <98232931+Crayonzla@users.noreply.github.com>
2023-07-06 15:00:11 -0700

    Gumball Enemy added as well as gumball

--- Funny Gamejam Project/enemy.gd
+++ Funny Gamejam Project/enemy.gd
@@ -1,2 +1,9 @@
 extends Area2D
 
+
+
+func _process(delta):
+	pass
+
+func _fire():
+	pass
---
+++ Funny Gamejam Project/machine.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+var Gumball = preload("res://gumball.tscn")
+
+func _fire():
+	var gumball = Gumball.instantiate()
+	var main = get_tree().current_scene
+	main.add_child(gumball)
+	gumball.position = $Marker2D.global_position
+
+
+func _on_timer_timeout():
+	$AnimationPlayer.play("fire")
+	
+func _on_animation_player_animation_finished(anim_name):
+	_fire()
